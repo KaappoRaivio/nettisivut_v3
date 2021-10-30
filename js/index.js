c848fc1f25ce8b2f6d5affe61c5e06bc7196e411
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-10-30 15:52:07 +0300

    autocommit

---
+++ js/index.js
@@ -0,0 +1,12 @@
+// console.log("moi");
+//
+//
+//
+// window.onresize = () => {
+//     const header = document.getElementById("header-wrapper");
+//
+//     const element = document.getElementById("top");
+//     element.style.scrollPaddingTop = `${header.getBoundingClientRect().height} px`;
+//     console.log(element.style.scrollPaddingTop)
+//
+// }
