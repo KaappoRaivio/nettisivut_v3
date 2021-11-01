a75f0835dcad444f75a8db07277221608bcfcbe3
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-11-01 14:13:51 +0200

    autocommit
    Took 2 hours 0 minutes

--- index.js
+++ index.js
@@ -22,34 +22,38 @@ const registerPartials = async () => {
         console.log(partialName)
         Handlebars.registerPartial(partialName, fs.readFileSync(templateFilePath, "utf-8"));
     })
-
-    // Handlebars.registerPartial("head", fs.readFileSync("templates/head.template.html", "utf-8"))
-    // Handlebars.registerPartial("header", fs.readFileSync("templates/header.template.html", "utf-8"))
-    // Handlebars.registerPartial("fancyGreeting", fs.readFileSync("templates/fancyGreeting.template.html", "utf-8"))
 }
 
 
-
+// await registerPartials();
 
 registerPartials().then(() => {
-    // if (!IS_PRODUCTION) {
+    app.use('/public', express.static(path.join(__dirname, "/public")))
+
     const mainTemplate = Handlebars.compile(fs.readFileSync("templates/index.template.html", "utf-8"));
-    // }
     app.get("/", (req, res) => {
-        if (!IS_PRODUCTION) {
-            Handlebars.c
-            const mainTemplate = Handlebars.compile(fs.readFileSync("templates/index.template.html", "utf-8"));
             res.status(200);
             res.send(mainTemplate(schema));
-        }
-
-        res.status(200);
-        res.send(mainTemplate(schema));
-        // console.log(mainTemplate(schema))
-        // res.html(mainTemplate(schema));
     })
 
-    app.use('/public', express.static(path.join(__dirname, "/public")))
+    const aboutTemplate = Handlebars.compile(fs.readFileSync("templates/about.template.html", "utf-8"));
+    app.get("/about", (req, res) => {
+        res.status(200);
+        res.send(aboutTemplate(schema));
+    })
+
+    const portfolioTemplate = Handlebars.compile(fs.readFileSync("templates/portfolio.template.html", "utf-8"));
+    app.get("/portfolio", (req, res) => {
+        res.status(200);
+        res.send(portfolioTemplate(schema));
+    })
+
+    const cvTemplate = Handlebars.compile(fs.readFileSync("templates/cv.template.html", "utf-8"));
+    app.get("/cv", (req, res) => {
+        res.status(200);
+        res.send(cvTemplate(schema));
+    })
+
     app.listen(8000, () => {
         console.log("listening on port 8000!")
     })
