15427aee4d594b7d473051c1d82be8f32df6370e
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-11-07 20:47:18 +0200

    update head
    Took 11 minutes

--- redirectToHttps.js
+++ redirectToHttps.js
@@ -10,5 +10,13 @@ module.exports = () => {
     } else return next();
   });
 
+  app.use((req, res, next) => {
+    if (req.headers.host.slice(0, 4) === "www.") {
+      var newHost = req.headers.host.slice(4);
+      return res.redirect(301, `https://${newHost}${req.originalUrl}`);
+    }
+    next();
+  });
+
   return app;
 };
