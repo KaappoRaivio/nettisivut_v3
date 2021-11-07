8f209934b3fd25da168dc830f8f8c582b871df51
Kaappo Raivio <kaappo.raivio@gmail.com>
2021-11-07 20:26:26 +0200

    autocommit
    Took 20 minutes

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const fs = require("fs");
 const redirectToHttps = require("./redirectToHttps");
 
 const getCertificates = debug => {
+  if (debug) return;
   const key = fs.readFileSync("/etc/letsencrypt/live/kaapporaivio.fi/privkey.pem", "utf8");
   const cert = fs.readFileSync("/etc/letsencrypt/live/kaapporaivio.fi/cert.pem", "utf8");
   const ca = fs.readFileSync("/etc/letsencrypt/live/kaapporaivio.fi/chain.pem", "utf8");
@@ -20,17 +21,16 @@ const main = async () => {
 
   const app = await makeApp(DEBUG);
 
-  const httpsServer = https.createServer(getCertificates(), app);
-  httpsServer.listen(process.env.PORT || 443, () => {
-    console.log(`listening on HTTPS!`);
-  });
-
   if (DEBUG) {
     const httpServer = http.createServer(app);
     httpServer.listen(process.env.PORT || 3000);
   } else {
     const httpServer = http.createServer(redirectToHttps());
     httpServer.listen(80);
+    const httpsServer = https.createServer(getCertificates(DEBUG), app);
+    httpsServer.listen(process.env.PORT || 443, () => {
+      console.log(`listening on HTTPS!`);
+    });
   }
 };
 
