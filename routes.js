03e053d2ad1575ca1a08007cb864010db8ff285d
Kaappo Raivio <kaappo.raivio@gmail.com>
2022-05-09 21:57:38 +0300

    autocommit

--- routes.js
+++ routes.js
@@ -15,6 +15,9 @@ module.exports = async app => {
   app.use("/.well-known", express.static(path.join(__dirname, "/.well-known")));
   app.use("/sitemap.xml", express.static(path.join(__dirname, "/sitemap.xml")));
   app.use("/robots.txt", express.static(path.join(__dirname, "/robots.txt")));
+  app.get("/sitemap.xml", (req, res) => {
+    res.sendFile(express.static(path.join(__dirname, "/sitemap.xml")));
+  });
 
   const boilerplateTemplate = await db.getBoilerplateTemplate();
 
